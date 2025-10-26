2bd7e5723db044338720558e4ebab322407a3135
GonzaloJCC <gonzalojimenezcarretero@gmail.com>
2025-10-27 00:26:17 +0100

    Modified params

--- models/LinskerModel.h
+++ models/LinskerModel.h
@@ -18,24 +18,20 @@ class LinskerModel {
  public:
     // static vector<precission> all_sinapsis_i; // Possible variable for normalization
     enum variable {
-      w,        // Synaptic weight (the stregth of the synapsis)
-      avg_pre,  // Average of the presynaptic neuron's voltage
-      avg_post, // Average of the postynaptic neuron's voltage
-      avg_corr, // Average of the correlation between pre and post voltages
+      w,        // Synaptic efficacy
       n_variables
     };
 
     enum parameter {
       v_pre,    // Presynaptic neuron's voltage
       v_post,   // Postsynaptic neuron's voltage
-      k1,       // Hebbian term coefficient
-      k2,       // Presynaptic bias term coefficient
-      k3,       // Postsynaptic bias term coefficient
-      k4,       // Constant term coefficient (often zero)
+      xo,       // Constant for presynaptic neuron
+      yo,       // Constant for postsynaptic neuron
+      eta,      // Learning rate
+      ki,       // Constant
       w_min,    // Minimum allowed synaptic weight
       w_max,    // Maximum allowed synaptic weight
       i,        // Synaptic intensity neuron
-      time_constant // Constant for the averages (must have the same units as step (ms, seconds, etc.))
       n_parameters
     };
 

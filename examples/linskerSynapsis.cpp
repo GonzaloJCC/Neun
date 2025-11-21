b99b4e83ec1994de68ecbec573f521573e0954ac
GonzaloJCC <gonzalojimenezcarretero@gmail.com>
2025-11-21 18:30:30 +0100

    Normalization done, i needs to be calculated

--- examples/linskerSynapsis.cpp
+++ examples/linskerSynapsis.cpp
@@ -30,48 +30,60 @@ int main(int argc, char **argv) {
 
 
   Synapsis::ConstructorArgs syn_args;
-  syn_args.params[Synapsis::xo] = 0.2;
+  syn_args.params[Synapsis::xo] = -90;
   syn_args.params[Synapsis::yo] = -50;
-  syn_args.params[Synapsis::eta] = 0.0005;
-  syn_args.params[Synapsis::k1] = 1;
-  syn_args.params[Synapsis::w_max] = 3;
+  syn_args.params[Synapsis::eta] = 0.00025;
+  syn_args.params[Synapsis::k1] = 50;
+  syn_args.params[Synapsis::w_max] = 5;
 
 
   // Initialize neuron models
   HH h1(args), h2(args);
+  HH h3(args);
 
   // Set initial value of V in neuron n1
   h1.set(HH::v, -75);
+  h3.set(HH::v, -72);
 
   // Set the integration step
   const double step = 0.005;
 
-  // Initialize a synapsis between the neurons
-  Synapsis s(h1, HH::v, h2, HH::v, syn_args, 1);
+  // Initialize 2 synapsis between the neurons (h2 will be post-synapstic neuron in both)
+  Synapsis s1(h1, HH::v, h2, HH::v, syn_args, 1);
+  Synapsis s2(h3, HH::v, h2, HH::v, syn_args, 1);
 
 
   // Perform the simulation
   double simulation_time = 10000;
-  std::cout << "Time" << " " << "Vpre" << " " << "Vpost" 
-              << " " << "i" << " " << "w" << std::endl;
+  // std::cout << "Time vpre vpost i w" << std::endl;
+  std::cout << "Time" << " " << "V1pre" << " " << "V2pre" << " " << "Vpost" 
+              << " " << "i1" << " " << "i2" << "w1" << " " << "w2" << " " << "SUM(W)" << std::endl;
 
   for (double time = 0; time < simulation_time; time += step) {
-    s.step(step, h1.get(HH::v), h2.get(HH::v));
+    s1.step(step, h1.get(HH::v), h2.get(HH::v));
+    s2.step(step, h3.get(HH::v), h2.get(HH::v));
 
     // Provide an external current input to both neurons
     h1.add_synaptic_input(0.5);
     h2.add_synaptic_input(0.5);
+    h3.add_synaptic_input(0.6);
 
-    h2.add_synaptic_input(s.get(Synapsis::i));
+    h2.add_synaptic_input(s1.get(Synapsis::i));
+    h2.add_synaptic_input(s2.get(Synapsis::i));
 
     h1.step(step);
+    h3.step(step);
     h2.step(step);
 
-    std::cout << time << " " << h1.get(HH::v) << " " << h2.get(HH::v) 
-              << " " << s.get(Synapsis::i)<< " " << s.get(Synapsis::w) << "\n";
+    std::cout << time << " " 
+              << h1.get(HH::v) << " " << h3.get(HH::v) << " " << h2.get(HH::v) << " " 
+              << s1.get(Synapsis::i) << " " << s2.get(Synapsis::i) << " " 
+              << s1.get(Synapsis::w) << " " << s2.get(Synapsis::w) << " "
+              << (s1.get(Synapsis::w) + s2.get(Synapsis::w))
+              << "\n";
+
+    // std::cout << time << " " << h1.get(HH::v) << " " << h2.get(HH::v) << " " << s1.get(Synapsis::i)<< " " << s1.get(Synapsis::w) << "\n";
   }
 
   return 0;
 }
-
-/* TODO: Add h3 and synapse s2 from h3 to h2*/
--- include/SynapseWeightNormalizer.h
+++ include/SynapseWeightNormalizer.h
@@ -60,60 +60,47 @@ class SynapseWeightNormalizer {
    */
   void normalize_weights(Synapse* updated_synapse, Neuron* post_synaptic_neuron) {
         
-    // 1. Obtener el grupo
     auto map_iterator = neuron_synapses_map.find(post_synaptic_neuron);
     if (map_iterator == neuron_synapses_map.end()) return;
 
     std::vector<Synapse*>& synapse_group = map_iterator->second;
     if (synapse_group.empty()) return;
+
     const precission w_max = updated_synapse->get_w_max();
     const precission w_min = -w_max;
-    precission current_weight = updated_synapse->get_weight();
+    precission range = w_max - w_min;
 
-    if (current_weight > w_max) {
-        current_weight = w_max;
-    } else if (current_weight < w_min) {
-        current_weight = w_min;
+    // Find the real range
+    precission current_max = synapse_group[0]->get_weight();
+    precission current_min = synapse_group[0]->get_weight();
+
+    for(Synapse* s : synapse_group) {
+      precission w = s-> get_weight();
+      if (w < current_min) current_min = w;
+      if (w > current_max) current_max = w;
     }
-    updated_synapse->set_weight(current_weight);
-    
-    // If only 1 synapse just return
-    if (synapse_group.size() <= 1) {
-        return; 
+    range = current_max - current_min;
+
+    if (std::abs(range) < 1e-9) {
+      for(Synapse* s : synapse_group) {
+            precission w = s->get_weight();
+            if (w > w_max) s->set_weight(w_max);
+            else if (w < -w_max) s->set_weight(-w_max);
+      }
+      return;
     }
 
-    //NORMALIZE:
-    
-    // 1. Calcular la suma actual de pesos
-    precission current_sum = 0;
+    // For all synapsis (except updated_synapse) do the normalization
     for (Synapse* s : synapse_group) {
-        current_sum += s->get_weight();
-    }
 
-    // Right now target sum = N * (w_max / 2)
-    precission target_sum = synapse_group.size() * (w_max / 2.0); 
-
-    precission delta = current_sum - target_sum;
-    precission adjustment = -delta / (synapse_group.size() - 1);
-
-    // Adjust all synapses (except the one we updated earlier)
-    for (Synapse* s : synapse_group) {
-        if (s == updated_synapse) continue;
-
-        precission new_w = s->get_weight() + adjustment;
+        precission w = s->get_weight();
         
-        // Limit them [-w_max, w_max]
-        if (new_w > w_max) {
-          new_w = w_max;
-        }
-        else if (new_w < w_min) {
-          new_w = w_min;
-        }
+        // w = (w - w.min()) / (w.max() - w.min()) * 2 * wmax - wmax 
+        precission new_w = (w - current_min) / (range) * 2 * w_max - w_max;
         
         s->set_weight(new_w);
     }
   }
-
  private:
 
  // Private constructor (Singleton)

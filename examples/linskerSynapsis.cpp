930208d41c72623d765fb43dee2a1d47db3ffa03
GonzaloJCC <gonzalojimenezcarretero@gmail.com>
2026-01-19 23:30:39 +0100

    Minor changes

--- examples/linskerSynapsis.cpp
+++ examples/linskerSynapsis.cpp
@@ -30,92 +30,128 @@ int main(int argc, char **argv) {
   args.params[HH::gl] = 0.3 * 7.854e-3;
 
 
+  // Synapsis::ConstructorArgs syn_args;
+  // syn_args.params[Synapsis::xo] = -65;
+  // syn_args.params[Synapsis::yo] = -65;
+  // syn_args.params[Synapsis::eta] = 0.00001;
+  // syn_args.params[Synapsis::k1] = -500;
+  // syn_args.params[Synapsis::w_max] = 3;
+
   Synapsis::ConstructorArgs syn_args;
   syn_args.params[Synapsis::xo] = -65;
-  syn_args.params[Synapsis::yo] = -65;
+  syn_args.params[Synapsis::yo] = -63;
   syn_args.params[Synapsis::eta] = 0.00001;
-  syn_args.params[Synapsis::k1] = -500;
-  syn_args.params[Synapsis::w_max] = 3;
+  syn_args.params[Synapsis::k1] = -50;
+  syn_args.params[Synapsis::w_max] = 2;
 
-
-  // Initialize neuron models
-  HH h1(args), h2(args);
-
-  HH h3(args);
-  HH h4(args);
-  HH h5(args);
-
-  // Set initial value of V in neuron n1
-  h1.set(HH::v, -75);
-  h3.set(HH::v, -71);
-  h4.set(HH::v, -70);
-  h5.set(HH::v, -78); 
 
   // Set the integration step
   const double step = 0.005;
-
-  // Initialize 2 synapsis between the neurons (h2 will be post-synapstic neuron in both)
-  Synapsis s1(h1, HH::v, h2, HH::v, syn_args, 1);
-
-  Synapsis s2(h3, HH::v, h2, HH::v, syn_args, 1);
-  // Synapsis s3(h4, HH::v, h2, HH::v, syn_args, 1);
-  // Synapsis s4(h5, HH::v, h2, HH::v, syn_args, 1);
-  
-  // Initialize weights with random values (to break symmetry)
-  s1.set_weight(0.001 * (rand() / (double)RAND_MAX));
-
-  s2.set_weight(0.001 * (rand() / (double)RAND_MAX));
-  // s3.set_weight(0.001 * (rand() / (double)RAND_MAX));
-  // s4.set_weight(0.001 * (rand() / (double)RAND_MAX));
-
-  // Perform the simulation
   double simulation_time = 10000;
-  // std::cout << "Time vpre vpost i w" << std::endl;
-  // std::cout << "Time" << " " << "V1pre" << " " << "V2pre" << " " << "V3pre" << " " << "V4pre" << " " << "Vpost" 
-  //             << " " << "i1" << " " << "i2" << " " << "i3" << " " << "i4" << " " << "w1" << " " << "w2" << " " << "w3" << " " << "w4" << " " << "SUM(W)" << std::endl;
 
-  std::cout << "Time v1pre v2pre vpost i1 i2 w1 w2" << std::endl;
+  // 3 Neurons 2 synapses
+  {
+      // Initialize neuron models
+      HH h1(args), h2(args);
+      HH h3(args);
 
-  for (double time = 0; time < simulation_time; time += step) {
-    s1.step(step, h1.get(HH::v), h2.get(HH::v));
+      // Set initial value of V
+      h1.set(HH::v, -75);
+      h3.set(HH::v, -71);
+      
+      // Initialize 2 synapsis
+      Synapsis s1(h1, HH::v, h2, HH::v, syn_args, 1);
+      Synapsis s2(h3, HH::v, h2, HH::v, syn_args, 1);
 
-    s2.step(step, h3.get(HH::v), h2.get(HH::v));
-    // s3.step(step, h4.get(HH::v), h2.get(HH::v));
-    // s4.step(step, h5.get(HH::v), h2.get(HH::v));
+      // Initialize weights
+      s1.set_weight(0.001 * (rand() / (double)RAND_MAX));
+      s2.set_weight(0.001 * (rand() / (double)RAND_MAX));
 
-    // Provide an external current input to all neurons
-    h1.add_synaptic_input(0.5);
-    h2.add_synaptic_input(0.5);
+      std::cout << "Time v1pre v2pre vpost i1 i2 w1 w2" << std::endl;
 
-    h3.add_synaptic_input(0.6);
-    // h4.add_synaptic_input(0.5);
-    // h5.add_synaptic_input(0.5);
+      for (double time = 0; time < simulation_time; time += step) {
+          s1.step(step, h1.get(HH::v), h2.get(HH::v));
+          s2.step(step, h3.get(HH::v), h2.get(HH::v));
 
-    h2.add_synaptic_input(s1.get(Synapsis::i));
-    h2.add_synaptic_input(s2.get(Synapsis::i));
-    // h2.add_synaptic_input(s3.get(Synapsis::i));
-    // h2.add_synaptic_input(s4.get(Synapsis::i));
+          // Inputs
+          h1.add_synaptic_input(0.5);
+          h2.add_synaptic_input(0.5);
+          h3.add_synaptic_input(0.6);
 
-    h1.step(step);
+          h2.add_synaptic_input(s1.get(Synapsis::i));
+          h2.add_synaptic_input(s2.get(Synapsis::i));
 
-    h3.step(step);
-    // h4.step(step);
-    // h5.step(step);
+          h1.step(step);
+          h3.step(step);
+          h2.step(step);
 
-    h2.step(step);
-
-    std::cout << time << " " << h1.get(HH::v) << " " << h3.get(HH::v) << " " << h2.get(HH::v) << " " << s1.get(Synapsis::i) << " " 
-              << s2.get(Synapsis::i) << " " << s1.get(Synapsis::w) << " " << s2.get(Synapsis::w) << " " 
-              << (s1.get(Synapsis::w) + s2.get(Synapsis::w)) << "\n";
-    // std::cout << time << " " 
-    //           << h1.get(HH::v) << " " << h3.get(HH::v) << " " << h4.get(HH::v) << " " << h5.get(HH::v) << " " << h2.get(HH::v) << " " 
-    //           << s1.get(Synapsis::i) << " " << s2.get(Synapsis::i) << " " << s3.get(Synapsis::i) << " " << s4.get(Synapsis::i) << " " 
-    //           << s1.get(Synapsis::w) << " " << s2.get(Synapsis::w) << " " << s3.get(Synapsis::w) << " " << s4.get(Synapsis::w) << " "
-    //           << (s1.get(Synapsis::w) + s2.get(Synapsis::w) + s3.get(Synapsis::w) + s4.get(Synapsis::w))
-    //           << "\n";
-
-    // std::cout << time << " " << h1.get(HH::v) << " " << h2.get(HH::v) << " " << s1.get(Synapsis::i)<< " " << s1.get(Synapsis::w) << "\n";
+          std::cout << time << " " << h1.get(HH::v) << " " << h3.get(HH::v) << " " << h2.get(HH::v) << " " 
+                    << s1.get(Synapsis::i) << " " << s2.get(Synapsis::i) << " " 
+                    << s1.get(Synapsis::w) << " " << s2.get(Synapsis::w) << " " 
+                    << (s1.get(Synapsis::w) + s2.get(Synapsis::w)) << "\n";
+      }
   }
+
+  // ----------------------------------------------- //
+  
+  // 5 Neurons 4 synapses
+//   {
+//       // Initialize neuron models
+//       HH h1(args), h2(args), h3(args), h4(args), h5(args);
+
+//       // Set initial value of v
+//       h1.set(HH::v, -75);
+//       h3.set(HH::v, -71);
+//       h4.set(HH::v, -70);
+//       h5.set(HH::v, -78);
+
+//       // Initialize 4 synapses (h2 is post-synaptic)
+//       Synapsis s1(h1, HH::v, h2, HH::v, syn_args, 1);
+//       Synapsis s2(h3, HH::v, h2, HH::v, syn_args, 1);
+//       Synapsis s3(h4, HH::v, h2, HH::v, syn_args, 1);
+//       Synapsis s4(h5, HH::v, h2, HH::v, syn_args, 1);
+
+//       // Initialize weights
+//       s1.set_weight(0.001 * (rand() / (double)RAND_MAX));
+//       s2.set_weight(0.001 * (rand() / (double)RAND_MAX));
+//       s3.set_weight(0.001 * (rand() / (double)RAND_MAX));
+//       s4.set_weight(0.001 * (rand() / (double)RAND_MAX));
+
+//       std::cout << "Time V1pre V2pre V3pre V4pre Vpost i1 i2 i3 i4 w1 w2 w3 w4 SUM(W)" << std::endl;
+
+//       for (double time = 0; time < simulation_time; time += step) {
+//           s1.step(step, h1.get(HH::v), h2.get(HH::v));
+//           s2.step(step, h3.get(HH::v), h2.get(HH::v));
+//           s3.step(step, h4.get(HH::v), h2.get(HH::v));
+//           s4.step(step, h5.get(HH::v), h2.get(HH::v));
+
+//           // External Inputs
+//           h1.add_synaptic_input(0.5);
+//           h2.add_synaptic_input(0.5);
+//           h3.add_synaptic_input(0.6);
+//           h4.add_synaptic_input(0.5);
+//           h5.add_synaptic_input(0.5);
+
+//           // Synaptic inputs to h2
+//           h2.add_synaptic_input(s1.get(Synapsis::i));
+//           h2.add_synaptic_input(s2.get(Synapsis::i));
+//           h2.add_synaptic_input(s3.get(Synapsis::i));
+//           h2.add_synaptic_input(s4.get(Synapsis::i));
+
+//           h1.step(step);
+//           h3.step(step);
+//           h4.step(step);
+//           h5.step(step);
+//           h2.step(step);
+
+//           std::cout << time << " " 
+//                     << h1.get(HH::v) << " " << h3.get(HH::v) << " " << h4.get(HH::v) << " " << h5.get(HH::v) << " " << h2.get(HH::v) << " " 
+//                     << s1.get(Synapsis::i) << " " << s2.get(Synapsis::i) << " " << s3.get(Synapsis::i) << " " << s4.get(Synapsis::i) << " " 
+//                     << s1.get(Synapsis::w) << " " << s2.get(Synapsis::w) << " " << s3.get(Synapsis::w) << " " << s4.get(Synapsis::w) << " "
+//                     << (s1.get(Synapsis::w) + s2.get(Synapsis::w) + s3.get(Synapsis::w) + s4.get(Synapsis::w))
+//                     << "\n";
+//       }
+//   }
 
   return 0;
 }

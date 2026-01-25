074b6cd9ffcaa1c813a00ff7918c7db638e53c59
GonzaloJCC <gonzalojimenezcarretero@gmail.com>
2026-01-25 20:22:11 +0100

    Song Miller Abbott Model in progress (almost done)

---
+++ examples/songMillerAbbotSynapse.cpp
@@ -0,0 +1,65 @@
+/*************************************************************
+
+
+*************************************************************/
+
+#include <DifferentialNeuronWrapper.h>
+#include <SongMillerAbbottSynapse.h>
+#include <HodgkinHuxleyModel.h>
+#include <SystemWrapper.h>
+#include <RungeKutta4.h>
+#include <iostream>
+#include <cstdlib>
+
+typedef RungeKutta4 Integrator;
+typedef DifferentialNeuronWrapper<SystemWrapper<HodgkinHuxleyModel<double>>, Integrator> HH;
+typedef SongMillerAbbottSynapse<HH, HH, Integrator, double> Synapse;
+
+int main(int argc, char **argv) {
+
+  // Struct to initialize neuron model parameters
+  HH::ConstructorArgs args;
+
+  // Set the parameter values
+  args.params[HH::cm] = 1 * 7.854e-3;
+  args.params[HH::vna] = 50;
+  args.params[HH::vk] = -77;
+  args.params[HH::vl] = -54.387;
+  args.params[HH::gna] = 120 * 7.854e-3;
+  args.params[HH::gk] = 36 * 7.854e-3;
+  args.params[HH::gl] = 0.3 * 7.854e-3;
+
+  // Struct to initialize synapse model parameters
+  Synapse::ConstructorArgs syn_args;
+  syn_args.params[Synapse::A_minus] = 0.02;
+  syn_args.params[Synapse::A_plus] = 0.005;
+  syn_args.params[Synapse::tau_minus] = 20;
+  syn_args.params[Synapse::tau_plus] = 20;
+  syn_args.params[Synapse::g_max] = 1;
+
+  // Set the integration step
+  const double step = 0.005;
+  double simulation_time = 10000;
+
+  // Initialize neuron models
+  HH h1(args), h2(args);
+
+  // Set initial value of V
+  h1.set(HH::v, -75);
+  
+  // Initialize synapse
+  Synapse s1(h1, HH::v, h2, HH::v, syn_args, 1);
+
+  std::cout << "Time vpre vpost i g" << std::endl;
+
+  for (double time = 0; time < simulation_time; time += step) {
+    s1.step(step, h1.get(HH::v), h2.get(HH::v));
+    h1.add_synaptic_input(0.5);
+    h2.add_synaptic_input(s1.get(Synapse::i));
+    h1.step(step);
+    h2.step(step);
+    std::cout << time << " " << h1.get(HH::v) << " " << h2.get(HH::v) << " " << s1.get(Synapse::i) << " " << s1.get(Synapse::g) << "\n";
+  }
+
+  return 0;
+}
---
+++ include/SongMillerAbbottSynapse.h
@@ -0,0 +1,155 @@
+/*************************************************************
+
+*************************************************************/
+
+#ifndef SONGMILLERABBOTTSYNAPSE_H_
+#define SONGMILLERABBOTTSYNAPSE_H_
+
+#ifndef __AVR_ARCH__
+#include <type_traits>
+#include <concepts>
+#include "IntegratorConcept.h"
+#include "NeuronConcept.h"
+#endif //__AVR_ARCH__
+
+#include "SongMillerAbbottSynapseModel.h"
+#include "IntegratedSystemWrapper.h"
+#include "SerializableWrapper.h"
+#include "SystemWrapper.h"
+#include <cmath>
+
+
+/**
+* Implements a synapse based on (Song, Miller & Abbott, 2000)
+*/
+
+template <typename TNode1, typename TNode2, typename TIntegrator, typename precission = double>
+requires NeuronConcept<TNode1> && NeuronConcept<TNode2> &&
+    IntegratorConcept<TIntegrator, SerializableWrapper<
+          SystemWrapper<SongMillerAbbottSynapseModel<precission> > > >
+
+class SongMillerAbbottSynapse : public SerializableWrapper<
+          SystemWrapper<SongMillerAbbottSynapseModel<precission> > > {
+ private:
+  #ifndef __AVR_ARCH__
+    static_assert(std::is_floating_point<precission>::value);
+  #endif  //__AVR_ARCH__
+
+  precission m_release_time;
+
+  TNode1 const &m_n1;
+  TNode2 &m_n2;
+
+  precission m_last_value_pre;
+
+  const typename TNode1::variable m_n1_variable;
+  const typename TNode2::variable m_n2_variable;
+
+
+  typedef SerializableWrapper<
+      SystemWrapper<SongMillerAbbottSynapseModel<precission> > > System;
+
+  const int m_steps;
+
+ public:
+  typedef typename System::precission_t precission_t;
+  typedef typename System::variable variable;
+  typedef typename System::parameter parameter;
+  typedef typename System::ConstructorArgs ConstructorArgs;
+
+  SongMillerAbbottSynapse (TNode1 const &n1, typename TNode1::variable v1,
+                                              TNode2 &n2, typename TNode2::variable v2, 
+                                              ConstructorArgs &args, int steps)
+      : m_n1(n1),
+        m_n2(n2),
+        m_n1_variable(v1),
+        m_n2_variable(v2),
+        System(args),
+        m_steps(steps) {
+          for(int i = 0; i < System::n_variables; i++) {
+            System::m_variables[i] = 0;
+          }
+        }
+
+  SongMillerAbbottSynapse (TNode1 const &n1, TNode2 &n2,
+                            SongMillerAbbottSynapse  const &synapse)
+      : m_n1(n1),
+        m_n2(n2),
+        m_n1_variable(synapse.m_n1_variable),
+        m_n2_variable(synapse.m_n2_variable),
+        m_steps(synapse.m_steps),
+        System(synapse) {
+          for(int i = 0; i < System::n_variables; i++) {
+            System::m_variables[i] = 0;
+          }
+        }
+
+  void step(precission h) {
+    
+    precission v_pre = m_n1.get(m_n1_variable);
+    System::m_parameters[System::v_pre] = v_pre;
+    precission v_post = m_n2.get(m_n2_variable);
+    System::m_parameters[System::v_post] = v_post;
+    
+    precission threshold = System::m_parameters[System::spike_threshold];
+
+    for (int i = 0; i < m_steps; ++i) {
+      TIntegrator::step(*this, h, System::m_variables, System::m_parameters);
+    }
+
+    /* LTP */
+    if (v_pre >= threshold && v_pre < threshold) {
+      
+      precission time_left = System::m_variables[System::time_left_pre];
+      if (time_left > 0) {
+        System::m_variables[System::g] += System::m_parameters[System::A_plus] * (time_left / System::m_parameters[System::tau_plus]);
+      }
+
+      System::m_variables[System::time_left_pre] = System::m_parameters[System::tau_plus];
+    }
+
+    /* LTD */
+    if (v_post >= threshold && v_post < threshold) {
+      
+      precission time_left = System::m_variables[System::time_left_post];
+      if (time_left > 0) {
+        System::m_variables[System::g] += System::m_parameters[System::A_minus] * (time_left / System::m_parameters[System::tau_minus]);
+      }
+
+      System::m_variables[System::time_left_post] = System::m_parameters[System::tau_minus];
+    }
+
+    /* Limit growth */
+    if (System::m_variables[System::g] > System::m_parameters[System::g_max]) {
+      System::m_variables[System::g] = System::m_parameters[System::g_max];
+    }
+
+    /* Limit decay */
+    if (System::m_variables[System::g] < 0) {
+      System::m_variables[System::g] = 0;
+    }
+
+  }
+
+  void step(precission h, precission vpre, precission vpost) {
+    System::m_parameters[System::v_pre] = vpre;
+    System::m_parameters[System::v_post] = vpost;
+
+    for (int i = 0; i < m_steps; ++i) {
+      TIntegrator::step(*this, h, System::m_variables, System::m_parameters);
+    }
+
+    /* Limit growth */
+    if (System::m_variables[System::g] > System::m_parameters[System::g_max]) {
+      System::m_variables[System::g] = System::m_parameters[System::g_max];
+    }
+
+    /* Limit decay */
+    if (System::m_variables[System::g] < 0) {
+      System::m_variables[System::g] = 0;
+    }
+
+  }
+
+
+#endif /*SONGMILLERABBOTTSYNAPSE_H_*/
